25ef64ce9240490cd84f58431328b279f507cd65
Angel Fernandez <angel.fdz@mail.com>
2021-03-25 23:25:40 -0600

    proyecto fallido
    arreglar en un futuro, index db no responde correctamente a la actualizacion de registros

--- 32-PROYECTO-CRMIndexedDB/js/editarcliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/editarcliente.js
@@ -1,26 +1,67 @@
 (function() {
 
-    let DB
+    // let DB
+    // const formulario = document.querySelector('#formulario')
+    let idCliente
 
     const nombreInput = document.querySelector('#nombre')
     const correoInput = document.querySelector('#email')
     const telefonoInput = document.querySelector('#telefono')
     const empresaInput = document.querySelector('#empresa')
 
+    
+
     document.addEventListener('DOMContentLoaded', () => {
         conectarDB()
 
+        // Actualizar el registro
+        form.addEventListener('submit', actualizarCliente)
+
         // verificar el id de la url
         const parametrosURL = new URLSearchParams(window.location.search)
-        const idCliente = parametrosURL.get('id')
+        idCliente = parametrosURL.get('id')
 
         if(idCliente){
             setTimeout(() => {
                 obtenerCliente(idCliente)
-            }, 1000)
+            }, 100)
         }
 
-    });
+      
+
+    })
+
+    function actualizarCliente(event){
+        event.preventDefault()
+
+        if(nombreInput.value === '' || correoInput === '' || empresaInput === '' || telefonoInput === ''){
+            imprimirAlerta('Todos los campos son obligatorio', 'error')
+
+            return
+        }
+
+          // Actualizar cliente
+          const clienteActualizado = {
+            nombre: nombreInput.value,
+            email: correoInput.value,
+            empresa: empresaInput.value,
+            telefono: telefonoInput,
+            id: Number(idCliente)
+        }
+
+        const transaction = DB.transaction(['crm'], 'readwrite')
+        const objectStore = transaction.objectStore('crm')
+
+        objectStore.put(clienteActualizado);
+
+        transaction.oncomplete = function(){
+            console.log('Editado correctamente')
+        }
+
+        transaction.onerror = function(){
+            console.log('Hubo un error')
+        }
+    }
 
     function obtenerCliente(id){
         const transaction = DB.transaction(['crm'], 'readwrite')
